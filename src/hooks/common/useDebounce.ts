76770d9e2dac21cb1375a84f65a9e3faac4419e5
heojeongbo <heojungbo@gmail.com>
2024-01-07 01:47:30 +0900

    feat: add devounce hook

---
+++ src/hooks/common/useDebounce.ts
@@ -0,0 +1,28 @@
+import { useEffect, useRef } from 'react';
+
+export function useDebounce<T extends (...args: any[]) => void>(
+  callback: T,
+  delay: number,
+): T {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  useEffect(() => {
+    return () => {
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+    };
+  }, []);
+
+  function debounceFunction(...args: Parameters<T>) {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  }
+
+  return debounceFunction as T;
+}
